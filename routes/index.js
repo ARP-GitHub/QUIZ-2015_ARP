28181bfa174ed6b774ef4c901afb31e79b62cc96
ARP <ANTONIO@ANTONIO-X201.sscc.ae.aena.es>
2015-07-08 23:20:47 +0200

    Primera página y favicon; se elimina ruta /users

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+﻿var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz' });
 });
 
 module.exports = router;
