90e9058d37fc24642b30e8cce48fe7005d0452fc
ARP <arpgithub@gmail.com>
2015-07-13 00:36:33 +0200

    Mejoras en la respuesta a la pregunta y correción nombre imagen author.jpg

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -5,9 +5,10 @@ exports.question = function(req, res) {
 
 // GET /quizes/answer
 exports.answer = function(req, res) {
-   if (req.query.respuesta === 'Roma'){
-      res.render('quizes/answer', {respuesta: 'Correcto'});
+   //if (req.query.respuesta === 'Roma'){
+   if (req.query.respuesta.trim().match(/^roma$/i)){
+       res.render('quizes/answer', {respuesta: 'Correcto'});
    } else {
-      res.render('quizes/answer', {respuesta: 'Incorrecto'});
+       res.render('quizes/answer', {respuesta: 'Incorrecto'});
    }
 };
