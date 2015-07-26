2b33a33a92f7155241ae4e683497dd641fa16e5c
ARP <arpgithub@gmail.com>
2015-07-26 16:25:42 +0200

    M7: Añadido búsqueda preguntas en rama busquedas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -18,12 +18,41 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes/
 exports.index = function(req, res) {
-   models.Quiz.findAll().then(
+   var busqueda = req.query.search;
+   // Por defecto se muestran todas las preguntas al dar a preguntas
+   if (!!busqueda) {
+	busqueda = '%' + busqueda + '%';
+	busqueda = busqueda.toLowerCase().trim();
+	//busqueda = busqueda.replace(' ','%');
+	busqueda = busqueda.replace(/\s/g,'%');
+   }
+   models.Quiz.findAll((busqueda) ? {where: ["lower(pregunta) like ?", busqueda], order:["pregunta"]} : {}).then(
 	function(quizes) {
 	    res.render('quizes/index.ejs', { quizes: quizes});
         }
    ).catch(function(error) { next(error);});
 };
+
+
+// Otra forma
+//      if (!!busqueda) {
+//	 busqueda = '%' + busqueda + '%';
+//	 busqueda = busqueda.toLowerCase().trim();
+//	 busqueda = busqueda.replace(/\s/g,'%');
+//	 models.Quiz.findAll({where: ["lower(pregunta) like ?", busqueda], order:["pregunta"]}).then(
+//	     function(quizes) {
+// 		res.render('quizes/index.ejs', { quizes: quizes});
+//             }
+//   	 ).catch(function(error) { next(error);});
+//      }
+//      else {
+//	  models.Quiz.findAll().then(
+//	     function(quizes) {
+//	       res.render('quizes/index.ejs', { quizes: quizes});
+//           }
+//        ).catch(function(error) { next(error);});
+//     }
+//};
 
 
 // GET /quizes/:id
@@ -35,9 +64,12 @@ exports.show = function(req, res) {
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
       var resultado = 'Incorrecto';
+      // En la respuesta se admite mayúsculas/minúsculas indistintamente
+      // Se eliminan espacios inicial y final si hubiera
+      // Se han de introducir los acentos correctos! (idioma español)
       var patt = new RegExp('^' + req.quiz.respuesta + '$', 'i');
       if (patt.exec(req.query.respuesta.trim())) {
-          resultado = 'Correcto';
+          resultado = 'Correcto'; 
       }
       res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
